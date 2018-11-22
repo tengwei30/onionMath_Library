078aa00a4d472b87b16cffa470b74d38d0a5c132
zhangsong <1923646840@qq.com>
2018-11-22 11:39:22 +0800

    合并

--- pages/bookdetail/index.js
+++ pages/bookdetail/index.js
@@ -55,7 +55,7 @@ Page({
       //     userId
       //   }
       // }).then(res => {
-        dd.redirect({
+        dd.redirectTo({
           url: `/pages/borrowSuccess/borrowSuccess?onionId=${onionId}&userId=${userId}`
         })
       // }).catch(err => {
